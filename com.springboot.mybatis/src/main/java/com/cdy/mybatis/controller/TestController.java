95e5eb9200eba78a51448c9efc8938d1f34b2d31
chendeyou@n22.com.cn <chendeyou@n22.com.cn>
2019-08-12 23:18:54 +0800

    整合mybatis

--- com.springboot.mybatis/src/main/java/com/cdy/mybatis/controller/TestController.java
+++ com.springboot.mybatis/src/main/java/com/cdy/mybatis/controller/TestController.java
@@ -1,5 +1,7 @@
 package com.cdy.mybatis.controller;
 
+import com.cdy.mybatis.dto.Test;
+import com.cdy.mybatis.service.TestService;
 import com.cdy.mybatis.util.Url;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -14,8 +16,17 @@ public class TestController {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    @Autowired
+    private TestService testService;
+
+
     @GetMapping(Url.TEST)
     private List<Map<String, Object>> test() {
         return  jdbcTemplate.queryForList("select * from test");
+    }
+
+    @GetMapping(Url.TEST_MAPPER)
+    private Test test1() {
+        return  testService.selectByPrimaryKey(4);
     }
 }
--- com.springboot.mybatis/src/main/java/com/cdy/mybatis/util/Url.java
+++ com.springboot.mybatis/src/main/java/com/cdy/mybatis/util/Url.java
@@ -3,4 +3,6 @@ package com.cdy.mybatis.util;
 public interface Url {
 
      String TEST = "/test";
+
+     String TEST_MAPPER = "/test/mapper";
 }
